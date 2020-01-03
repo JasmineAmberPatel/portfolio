97008d0f7a8c26da5d34f52238ad0cf1559454dc
Jasmine Amber <jasmineamber.ja@gmail.com>
2020-01-03 22:26:16 +0000

    started homepage

---
+++ src/components/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home () {
+    return (
+        <div className="Home">
+            <img className="Home_Background" src={require('../images/clouds.jpg')} alt="background"></img>
+            <h1 className="Home_Title">Jasmine Amber Patel</h1>
+            <h2 className="Home_Title">Software Engineer</h2>
+        </div>
+    )
+};
+
+
+export default Home;
