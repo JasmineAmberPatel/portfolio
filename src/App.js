d6bd92c53a2663d3222961627fb5f91625a1988c
Jasmine Amber <jasmineamber.ja@gmail.com>
2020-01-04 15:15:31 +0000

    added background image and navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/main.css';
 import Home from './components/home';
+import Navbar from './components/navbar';
+import Projects from './components/projects';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Home />
+      <Projects />
     </div>
   );
 }
---
+++ src/components/projects.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Projects () {
+    return (
+        <div className="Projects">
+            <h1>Projects</h1>
+            <div>
+                <h2>Green Queens</h2>
+                
+                <p className="Project_Description">This project was built using react and includes an API integration to a website which lets you plant a tree to offset your carbon footprint and a calculator which calculates the volume of CO2 and water it took to produce the clothes you have bought in a year. You can see a video of me presenting this website here: https://www.youtube.com/watch?v=uRCBi5Ffd9A&t=8s my presentation starts at 12 minutes.</p>
+            </div>
+        </div>
+    )
+};
+
+
+export default Projects;
