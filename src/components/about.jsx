bfe5d82e5c7cec2ba9c660504e2633c3eba799e2
Jasmine Amber <jasmineamber.ja@gmail.com>
2020-01-22 20:15:48 +0000

    changed order of tech logos

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -10,10 +10,10 @@ function About() {
             <p className="Paragraph about">Hello! I am a 25 year old software developer based in the north west of England. On this website you can see a variety of different personal projects i have worked on, feel free to have a look around and get inspired.</p>
             <br />
             <div className="tech-logos">
-                <img className="logos" src={require('../images/react-01.png')} alt="react logo"></img>
                 <img className="logos" src={require('../images/js-01.png')} alt="js logo"></img>
                 <img className="logos" src={require('../images/html-01.png')} alt="html logo"></img>
                 <img className="logos" src={require('../images/css-01.png')} alt="css logo"></img>
+                <img className="logos" src={require('../images/react-01.png')} alt="react logo"></img>
                 <img className="logos" src={require('../images/sass-01.png')} alt="sass logo"></img>
                 <img className="logos" src={require('../images/node-01.png')} alt="node logo"></img>
             </div>
