cca73ed24b294a7de8d14c9c7c13b11813f0912e
Jasmine Amber <jasmineamber.ja@gmail.com>
2020-01-23 11:52:28 +0000

    made the page more responsive for inbetween mobile and desktop screen sizes

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -4,8 +4,8 @@ function Projects() {
     return (
         <div className="Projects" id="projects">
             <br />
-            <h2 className="Subtitle">Projects</h2> 
-            <br/>
+            <h2 className="Subtitle">Projects</h2>
+            <br />
             <div className="Projects_container">
                 <div className="Projects_box">
                     <img className="Projects_image gq" src={require('../images/gq-cover.png')} alt="green queens"></img>
@@ -22,11 +22,11 @@ function Projects() {
                     </div>
                 </div>
                 <div className="Projects_box">
-                <img className="Projects_image queens" src={require('../images/queens.png')} alt="quotes from queens"></img>
+                    <img className="Projects_image queens" src={require('../images/queens.png')} alt="quotes from queens"></img>
                     <div className="Project-info queens-info">
                         <h2 className="Subtitle">Quotes From Queens</h2>
-                        <p className="Paragraph">I built an Express API containing quotes from inspirational female artists and celebrities using MongoDB as the database. 
-                        I deployed the API using Heroku i then created a frontend using React and styled it using CSS to display the API information. 
+                        <p className="Paragraph">I built an Express API containing quotes from inspirational female artists and celebrities using MongoDB as the database.
+                        I deployed the API using Heroku i then created a frontend using React and styled it using CSS to display the API information.
                         I deployed the frontend using Zeit.</p>
                         <div className="buttons">
                             <a className="btn btn-dark" href="https://github.com/JasmineAmberPatel/Quotes-From-Queens" target="_blank" rel="noopener noreferrer">View Backend</a>
@@ -36,7 +36,7 @@ function Projects() {
                     </div>
                 </div>
                 <div className="Projects_box  weather">
-                <img className="Projects_image" src={require('../images/weather.png')} alt="weather app"></img>
+                    <img className="Projects_image" src={require('../images/weather.png')} alt="weather app"></img>
                     <div className="Project-info weather-info">
                         <h2 className="Subtitle">Weather Forecast</h2>
                         <p className="Paragraph">A 5 day weather forecasting app built with React with the data coming from a Web API. You can search to see the weather at any UK destination. The styling was done using CSS.</p>
@@ -47,7 +47,7 @@ function Projects() {
                     </div>
                 </div>
                 <div className="Projects_box golden">
-                <img className="Projects_image" src={require('../images/golden-shoe.png')} alt="golden shoe"></img>
+                    <img className="Projects_image" src={require('../images/golden-shoe.png')} alt="golden shoe"></img>
                     <div className="Project-info gq-info">
                         <h2 className="Subtitle">Golden Shoe</h2>
                         <p className="Paragraph">Golden Shoe is a prototype of an ecommerce website created with react and styled with bootstrap and sass.
@@ -59,7 +59,7 @@ function Projects() {
                     </div>
                 </div>
                 <div className="Projects_box music">
-                <img className="Projects_image" src={require('../images/music.png')} alt="golden shoe"></img>
+                    <img className="Projects_image" src={require('../images/music.png')} alt="golden shoe"></img>
                     <div className="Project-info gq-info">
                         <h2 className="Subtitle">Music Library API</h2>
                         <p className="Paragraph">This CRUD REST API stores information about artists, albums and songs and interacts with a MongoDB database using Express.
@@ -71,10 +71,29 @@ function Projects() {
                     </div>
                 </div>
                 <div className="Projects_box estate">
-                <img className="Projects_image" src={require('../images/property.png')} alt="golden shoe"></img>
+                    <img className="Projects_image" src={require('../images/property.png')} alt="golden shoe"></img>
                     <div className="Project-info gq-info">
                         <h2 className="Subtitle">Surreal Estate</h2>
-                        <p className="Paragraph">A property search engine created with react. Properties come from a web API. The homepage allows you to add a property and the view properties page allows you to view all properties which have been created. 
+                        <p className="Paragraph">A property search engine created with react. Properties come from a web API. The homepage allows you to add a property and the view properties page allows you to view all properties which have been created.
+                        </p>
+                        <div className="buttons">
+                            <a className="btn btn-dark" href="https://github.com/JasmineAmberPatel/surreal-estate-api" target="_blank" rel="noopener noreferrer">View Source</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="Projects_box pet">
+                    <img className="Projects_image" src={require('../images/cat.png')} alt="cat"></img>
+                    <div className="Project-info pet-info">
+                        <h2 className="Subtitle">Virtual Pet</h2>
+                        <p className="Paragraph">Real pets are great, but they have drawbacks. They smell. Your mum might be allergic to them.
+                    To solve these problems, I built my own JavaScript pet instead.
+                    This pet has all the following features:
+                    It can get older, as it gets older, it gets hungrier and less fit.
+                    You can walk it to increase it's fitness.
+                    You can feed it to decrease it's hunger.
+                    You can talk to it to see if it needs feeding or walking.
+                    If it gets too hungry or unfit, it will DIE.
+I                   If it gets to 30 days old it will DIE.
                         </p>
                         <div className="buttons">
                             <a className="btn btn-dark" href="https://github.com/JasmineAmberPatel/surreal-estate-api" target="_blank" rel="noopener noreferrer">View Source</a>
