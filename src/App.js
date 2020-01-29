4b63a7f4d294e82e79dc1b2ab6361734afb52513
Jasmine Amber <jasmineamber.ja@gmail.com>
2020-01-29 17:02:33 +0000

    added spotify now playing integration

--- src/App.js
+++ src/App.js
@@ -3,16 +3,70 @@ import './styles/main.scss';
 import About from './components/about';
 import Projects from './components/projects';
 import Social from './components/social';
+import SpotifyWebApi from 'spotify-web-api-js';
 
+const spotifyApi = new SpotifyWebApi();
 
-function App() {
-  return (
-    <div className="app">
-      <About />
-      <Projects />
-      <Social />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    const params = this.getHashParams();
+    const token = params.access_token;
+    if (token) {
+      spotifyApi.setAccessToken(token);
+    }
+    this.state = {
+      loggedIn: token ? true : false,
+      nowPlaying: { name: 'Not Checked', albumArt: '' }
+    }
+    console.log(params);
+  }
+  getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    e = r.exec(q)
+    while (e) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+      e = r.exec(q);
+    }
+    return hashParams;
+  }
+
+  getNowPlaying(){
+    spotifyApi.getMyCurrentPlaybackState()
+      .then((response) => {
+        this.setState({
+          nowPlaying: { 
+              name: response.item.name, 
+              albumArt: response.item.album.images[0].url
+            }
+        });
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <About />
+        <a className="spotify" href='http://localhost:8888' > Login to Spotify </a>
+        <div>
+          Now Playing: {this.state.nowPlaying.name}
+        </div>
+        <div>
+          <img src={this.state.nowPlaying.albumArt} style={{ height: 150 }} />
+        </div>
+        { this.state.loggedIn &&
+        <button onClick={() => this.getNowPlaying()}>
+          Check Now Playing
+        </button>
+      }
+        <Projects />
+        <Social />
+      </div>
+    );
+  }
 }
+
 
 export default App;
